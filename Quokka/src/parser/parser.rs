3f2a152098c1f4a0f987c0c4627889505bbda99d
ImTheCurse <raniking15@gmail.com>
2024-04-10 01:12:01 +0300

    Feat: Added Precdence enum + helper functions, created parse_expr(),parse_expr_statments and specific operator parse methods.

--- Quokka/src/parser/parser.rs
+++ Quokka/src/parser/parser.rs
@@ -1,6 +1,9 @@
 use crate::token::token::{Token, TokenType};
 use crate::Lexer;
-use crate::AST::ast::{Expression, Identifier, LetStatment, Literal, Program, Statment};
+use crate::AST::ast::{
+    Expression, ExpressionStatment, Identifier, LetStatment, Literal, Program, Statment,
+};
+use c_enum::c_enum;
 use std::fmt::Write;
 
 #[derive(Clone)]
@@ -9,6 +12,19 @@ pub struct Parser {
     pub curr_token: Token,
     pub peek_token: Token,
     pub errors: Vec<String>,
+}
+
+c_enum! {
+#[derive(PartialEq, Clone, Copy, Eq)]
+    pub enum Precedence :i32 {
+        Lowest = 1,
+        Equals,      //==
+        LessGreater, //< or >
+        Sum,         // + or -
+        Product,     //*
+        Call,        //func(x)
+
+    }
 }
 
 impl<'a> Parser {
@@ -27,6 +43,29 @@ impl<'a> Parser {
         p.next_token_parser();
 
         p
+    }
+
+    fn curr_token_is(&self, tok: &TokenType) -> bool {
+        self.curr_token.tok_type == *tok
+    }
+
+    fn token_to_precedence(tok: &TokenType) -> Precedence {
+        match *tok {
+            TokenType::EQ | TokenType::NotEQ => Precedence::Equals,
+            TokenType::Rarrow | TokenType::Larrow => Precedence::LessGreater,
+            TokenType::Plus | TokenType::Minus => Precedence::Sum,
+            TokenType::Asterisk | TokenType::Fslash => Precedence::Product,
+            TokenType::Lparen => Precedence::Call,
+            _ => Precedence::Lowest,
+        }
+    }
+
+    fn next_token_precedence(&mut self) -> Precedence {
+        Self::token_to_precedence(&self.peek_token.tok_type)
+    }
+
+    fn next_token_is(&self, tok: &TokenType) -> bool {
+        self.peek_token.tok_type == *tok
     }
 
     pub fn next_token_parser(&mut self) {
@@ -53,14 +92,49 @@ impl<'a> Parser {
         return match curr_tok.tok_type {
             TokenType::Let => self.parse_let_statment(),
             TokenType::Return => self.parse_return_statments(),
-            TokenType::Ident => self.parse_expr_statments(None),
-            TokenType::Int(num) => self.parse_expr_statments(Some(num)),
+            TokenType::Illegal => None,
+            _ => self.parse_expr_statments(),
 
             _ => None,
         };
     }
 
-    fn parse_expr_statments(&mut self, num: Option<i32>) -> Option<Statment> {
+    fn parse_expr_statments(&mut self) -> Option<Statment> {
+        return match self.parse_expr(Precedence::Lowest) {
+            Some(expr) => {
+                if self.next_token_is(&TokenType::Semicolon) {
+                    self.next_token_parser();
+                }
+                Some(Statment::Expression(ExpressionStatment { expr: expr }))
+            }
+            None => None,
+        };
+    }
+
+    fn parse_expr(&mut self, prec: Precedence) -> Option<Expression> {
+        // prefix
+        let mut lhs = match self.curr_token.tok_type {
+            TokenType::Ident => self.parse_ident(),
+            TokenType::Int(num) => self.parse_int(num),
+            _ => self.prefix_error(),
+        };
+
+        //infix
+
+        while !self.next_token_is(&TokenType::Semicolon) && prec < self.next_token_precedence() {
+            todo!()
+        }
+    }
+
+    fn parse_int(&mut self, num: i32) -> Option<Statment> {
+        todo!()
+    }
+
+    fn parse_ident(&mut self) -> Option<Statment> {
+        todo!()
+    }
+
+    fn prefix_error(&mut self) -> Option<Statment> {
         todo!()
     }
 
